1a14eac23cc1efe68ed2052b5ec67587094e4af1
rajapattanayak <pattanayak.raja@gmail.com>
2018-03-19 12:01:51 +0530

    Another demo template

--- server.js
+++ server.js
@@ -50,6 +50,13 @@ app.get('/about', (req, res) => {
     });
 })
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        projectDetails: '1) NodeJs Demo 2) Webserver'
+    });
+})
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: "unable to handel request"
